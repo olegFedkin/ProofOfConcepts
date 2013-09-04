b1099734f196229db0fcbadb6d1e076ca75a1db9
olegFedkin <olex4x@gmail.com>
2013-09-04 08:56:57 +0300

    removed HelloIndia and initHelloWorld

--- First_Spring_App/src/main/java/lv/fedkin/oleg/MainApp.java
+++ First_Spring_App/src/main/java/lv/fedkin/oleg/MainApp.java
@@ -1,18 +1,15 @@
 package lv.fedkin.oleg;
 
-import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
     public static void main(String[] args) {
-        AbstractApplicationContext context =
+        ApplicationContext context =
                 new ClassPathXmlApplicationContext("Beans.xml");
 
-        HelloIndia objB = (HelloIndia) context.getBean("helloIndia");
-        objB.getMessage1();
-        objB.getMessage2();
-        objB.getMessage3();
-
-        context.registerShutdownHook();
+        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+        obj.getMessage1();
+        obj.getMessage2();
     }
 }
